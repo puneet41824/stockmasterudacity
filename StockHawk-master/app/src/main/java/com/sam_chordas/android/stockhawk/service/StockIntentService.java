53a594141388f96d3457ee253f9f082dafd14418
puneet <puneet41824@gmail.com>
2016-07-16 23:22:59 +0530

    stockhawk project

---
+++ StockHawk-master/app/src/main/java/com/sam_chordas/android/stockhawk/service/StockIntentService.java
@@ -0,0 +1,49 @@
+package com.sam_chordas.android.stockhawk.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.gcm.GcmNetworkManager;
+import com.google.android.gms.gcm.TaskParams;
+import com.sam_chordas.android.stockhawk.R;
+
+/**
+ * Created by sam_chordas on 10/1/15.
+ */
+public class StockIntentService extends IntentService {
+
+    public StockIntentService() {
+        super(StockIntentService.class.getName());
+    }
+
+    public StockIntentService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d(StockIntentService.class.getSimpleName(), "Stock Intent Service");
+        StockTaskService stockTaskService = new StockTaskService(this);
+        Bundle args = new Bundle();
+        if (intent.getStringExtra("tag").equals("add")) {
+            args.putString("symbol", intent.getStringExtra("symbol"));
+        }
+        // We can call OnRunTask from the intent service to force it to run immediately instead of
+        // scheduling a task.
+        int result = stockTaskService.onRunTask(new TaskParams(intent.getStringExtra("tag"), args));
+
+        if (result == GcmNetworkManager.RESULT_FAILURE) {
+            Handler mHandler = new Handler(getMainLooper());
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.invalid_symbol), Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+}
